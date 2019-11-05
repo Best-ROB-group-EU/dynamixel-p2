666f61aca3bfd1dddadaca9d4c2598829d90be1b
fadeto404 <rskjel18@student.aau.dk>
2019-11-05 09:59:05 +0100

    Added constructor, begin() method, made changes to constructing and transmitting packets

--- src/dynamixel_p2/dynamixel_p2.h
+++ src/dynamixel_p2/dynamixel_p2.h
@@ -3,6 +3,8 @@
 #ifndef DYNAMIXEL_P2_DYNAMIXEL_P2_H
 #define DYNAMIXEL_P2_DYNAMIXEL_P2_H
 
+#include <Arduino.h>
+
 #include <iostream>
 #include <iomanip>
 // Contains adresses of the RAM control table
@@ -18,26 +20,33 @@ using namespace std;
 
 class Dynamixel_p2 {
 private:
+    // ATTRIBUTES
+    Stream *_serialport;
+    int _flow_control_pin = 13;
+    // METHODS
     void CreateHeader(unsigned char *tx_packet){}; // Function to build header (FF FF FD 00)
     void CreateId(unsigned char *tx_packet, unsigned char id){}; // Function to add ID to package
     unsigned char CreateInstruction(unsigned char *tx_packet unsigned char instruction, unsigned char parameters){};
     int CreateLength(unsigned char *tx_packet int parameters_size){};
     //void InsertBytes(unsigned char *tx_packet[], int position){}; // Function to insert an arbitrary amount of bytes at position in array
-    void PacketConstructor(unsigned char *tx_packet, unsigned char device_id, unsigned char instruction,
-                           unsigned char *params){};
+    void ConstructPacket(unsigned char *tx_packet, unsigned char device_id, unsigned char instruction,
+                         unsigned char *params){}; // Function that constructs packets, given id, instr, and parameters
     void TransmitPacket(unsigned char *tx_packet){}; // Function to send a package
     void ReceiveStatusPacket(){}; // Function to read the contents of received packages
 
     unsigned short update_crc (unsigned short crc_accum, unsigned char *data_blk_ptr, unsigned short data_blk_size); // Calculates CRC
     unsigned short data_blk_size(unsigned char *packet); // Calculates the block size for use in CRC Calculation
-    void unint16to8 (unsigned long value, unsigned char *package, unsigned char startParameters); // Function designed to split up a 32 Bit value in a 4x8 bit array.
-    void unint16to8 (unsigned long value, unsigned char *package, unsigned char startParameters); // 16it to 2x8 bit.
+    void uint32to8 (unsigned long value, unsigned char *package, unsigned char startParameters); // Function designed to split up a 32 Bit value in a 4x8 bit array.
+    void uint16to8 (unsigned long value, unsigned char *package, unsigned char startParameters); // 16it to 2x8 bit.
+
 public:
+    // UTILITY
+    Dynamixel_p2(int flow_control_pin = 13){};
+    void begin(long baud_rate = 57600){};
     // SETTERS
     void setTorqueEnable(){};
     // GETTERS
     void getTorqueEnable(){};
-
 };
 #endif //DYNAMIXEL_P2_DYNAMIXEL_P2_H
 
