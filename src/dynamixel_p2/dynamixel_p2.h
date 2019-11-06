261be99bbde1661e02fddb99022e453895317af5
fadeto404 <rskjel18@student.aau.dk>
2019-11-06 11:15:47 +0100

    Added method for receiving packets (work-in-progress)

--- src/dynamixel_p2/dynamixel_p2.h
+++ src/dynamixel_p2/dynamixel_p2.h
@@ -1,7 +1,7 @@
 // Created by 19gr362 Aalborg University, Robotics third semester.
 #define WRITE 0x03
 #define READ 0x02
-#define PING 0x01;
+#define PING 0x01
 #ifndef DYNAMIXEL_P2_DYNAMIXEL_P2_H
 #define DYNAMIXEL_P2_DYNAMIXEL_P2_H
 
@@ -25,25 +25,31 @@ private:
     // ATTRIBUTES
     Stream *_serialport;
     int _flow_control_pin = 13;
+    struct status_packet_info{
+        unsigned char id;
+        unsigned char error;
+        unsigned char parameters[4];
+    };
     // METHODS
     unsigned char Dynamixel_p2::CreateblkSize (unsigned char instruction, unsigned char address){};
     void CreateHeader(unsigned char *tx_packet){}; // Function to build header (FF FF FD 00)
     void CreateId(unsigned char *tx_packet, unsigned char id){}; // Function to add ID to package
     unsigned char CreateInstruction(unsigned char *tx_packet unsigned char instruction, unsigned char parameters){};
-    int CreateLength(unsigned char *tx_packet int parameters_size){};
+    unsigned short CreateLength(unsigned char *tx_packet int parameters_size){};
     //void InsertBytes(unsigned char *tx_packet[], int position){}; // Function to insert an arbitrary amount of bytes at position in array
     void ConstructPacket(unsigned char *tx_packet, unsigned char device_id, unsigned char instruction,
                          unsigned char *params){}; // Function that constructs packets, given id, instr, and parameters
     void TransmitPacket(unsigned char *tx_packet){}; // Function to send a package
-    void ReceiveStatusPacket(){}; // Function to read the contents of received packages
+    status_packet_info ReceiveStatusPacket(){}; // Function to read the contents of received packages
 
     unsigned short update_crc (unsigned short crc_accum, unsigned char *data_blk_ptr, unsigned short data_blk_size); // Calculates CRC
     unsigned short data_blk_size(unsigned char *packet); // Calculates the block size for use in CRC Calculation
     void Create4Params (unsigned long value, unsigned char *package, unsigned char startParameters); // Function designed to split up a 32 Bit value in a 4x8 bit array.
     void Create2Params (unsigned long value, unsigned char *package, unsigned char startParameters); // 16it to 2x8 bit.
 public:
-    // UTILITY
+    // CONSTRUCTORS
     Dynamixel_p2(int flow_control_pin = 13){};
+    // UTILITY
     void begin(long baud_rate = 57600){};
     // SETTERS
     void setTorqueEnable(){};
